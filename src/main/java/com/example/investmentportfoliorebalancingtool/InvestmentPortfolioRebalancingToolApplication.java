fe7f8c4bfae2ff2fae31a8a3b4342fa2a4c6779c
shadow2105 <ashwinrajput66@gmail.com>
2023-07-03 11:21:39 -0400

    Initial Project Add

---
+++ src/main/java/com/example/investmentportfoliorebalancingtool/InvestmentPortfolioRebalancingToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.investmentportfoliorebalancingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioRebalancingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentPortfolioRebalancingToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/investmentportfoliorebalancingtool/InvestmentPortfolioRebalancingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investmentportfoliorebalancingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioRebalancingToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
